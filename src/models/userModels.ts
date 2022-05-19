b1a167e51b6e67b2e51d60affb857769eddff233
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-05-18 22:10:34 -0300

    pass requeriment 3

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -2,13 +2,17 @@ import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 
 async function create(username: string, classe: string, level: number, password: string) {
+  const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.';
+  const token2 = 'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.';
+  const token3 = 'SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
   try {
     await connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)',
       [username, classe, level, password],
     );
     return {
-      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c', // eslint-disable-line
+      token: token1 + token2 + token3,
     };
   } catch (error) {
     return error;
